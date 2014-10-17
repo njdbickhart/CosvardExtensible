9d605eb100a09c3e07fe45fb69b3404a603d3ac6
njdbickhart <njdbickhart@gmail.com>
2014-10-17 12:57:45 -0400

    change in cmdopt class

---
+++ src/modules/CosvardModule.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modules;
+
+/**
+ *
+ * @author bickhart
+ */
+public abstract class CosvardModule {
+    private final ExecPaths paths;
+    private final LogFile logger;
+    
+    
+}
